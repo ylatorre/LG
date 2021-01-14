e0091dd8f758e3c2b16d30ac72ba47b47e7d2e6c
yvan <yvanlatorre@hotmail.fr>
2021-01-14 17:59:29 +0100

    Correction LG

--- lg.py
+++ lg.py
@@ -1,7 +1,7 @@
 import random
 from clear_screen import clear
 import time
-role_fix= ["Chasseur","Cupidon","Petite_Fille","Sorciere","Voyante"]
+role_fix = ["Chasseur", "Cupidon", "Petite_Fille", "Sorciere", "Voyante"]
 
 nbre_joueur = int(input("Entrez le Nombre de joueurs : "))
 nbre_mechant = nbre_joueur*0.4
@@ -9,19 +9,23 @@ nbre_mechant = int(nbre_mechant)
 print(nbre_mechant, nbre_joueur)
 
 i = 0
-listeJ = ["Chasseur","Cupidon","Petite_Fille","Sorciere","Voyante"]
+listeJ = ["Chasseur", "Cupidon", "Petite_Fille", "Sorciere", "Voyante"]
+
+
 def joueur():
     for i in range(nbre_mechant):
         listeJ.append('LG{}'.format(i+1))
     if nbre_joueur > 9:
-        vilagois = nbre_joueur - len(listeJ)
-        print("nombre de {} vilagois ".format(vilagois))
-        for i in range(vilagois):
-            listeJ.append('vilagois{}'.format(i+1))
+        villageois = nbre_joueur - len(listeJ)
+        print("nombre de {} villageois ".format(villageois))
+        for i in range(villageois):
+            listeJ.append('villageois{}'.format(i+1))
     random.shuffle(listeJ)
     print(listeJ)
     print(len(listeJ))
     return listeJ
+
+
 joueur()
 
 wait = input("Press Enter to continue.")
@@ -38,16 +42,15 @@ Equipe_LG = []
 for i in range(nbre_mechant):
     Equipe_LG.append(listeJ.index("LG{}".format(i+1))+1)
 Equipe_LG.sort()
-print('Les LG sont :',Equipe_LG)
+print('Les LG sont :', Equipe_LG)
 
 
-vilagois = nbre_joueur - len(role_fix)-nbre_mechant
+villageois = nbre_joueur - len(role_fix)-nbre_mechant
 Equipe_Villagois = []
-for i in range(vilagois):
-    Equipe_Villagois.append(listeJ.index("vilagois{}".format(i+1))+1)
+for i in range(villageois):
+    Equipe_Villagois.append(listeJ.index("villageois{}".format(i+1))+1)
 Equipe_Villagois.sort()
-print('Les villagois sont :',Equipe_Villagois)
-
+print('Les villagois sont :', Equipe_Villagois)
 
 
 print("Debut de la 1er nuit, le village s'endors.")
@@ -56,7 +59,7 @@ print("Debut de la 1er nuit, le village s'endors.")
 amoureux = input("Cupidon designe 2 joueurs :").split()
 amoureux.append(listeJ.index('Cupidon')+1)
 while len(amoureux) != 3:
-     amoureux = input("Erreur Cupidon designe 2 joueurs :").split()
-     amoureux.append(listeJ.index('Cupidon')+1)
-print("Les amoureux sont :",amoureux)
+    amoureux = input("Erreur Cupidon designe 2 joueurs :").split()
+    amoureux.append(listeJ.index('Cupidon')+1)
+print("Les amoureux sont :", amoureux)
 print('Fin de la 1er nuit')
